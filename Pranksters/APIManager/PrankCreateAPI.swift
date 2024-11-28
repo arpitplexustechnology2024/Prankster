15ef086f9b67330150d4673668dcee868d52570a
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-11-27 18:25:16 -0800

    Commit

---
+++ Pranksters/APIManager/PrankCreateAPI.swift
@@ -0,0 +1,8 @@
+//
+//  PrankCreateAPI.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 27/11/24.
+//
+
+import Foundation
---
+++ Pranksters/ViewModel/PrankCreateViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PrankCreateViewModel.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 27/11/24.
+//
+
+import Foundation
