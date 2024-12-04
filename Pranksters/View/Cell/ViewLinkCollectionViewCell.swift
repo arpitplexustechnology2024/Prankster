cd81657dc3fd775884d2bd67c1c24c9852c9067d
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-12-04 09:21:45 -0800

    Commit

---
+++ Pranksters/View/Cell/ViewLinkCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ViewLinkCollectionViewCell.swift
+//  Pranksters
+//
+//  Created by Arpit iOS Dev. on 01/12/24.
+//
+
+import UIKit
+
+class ViewLinkCollectionViewCell: UICollectionViewCell {
+    
+}
