ab542c3eb94732224bf7d1cb2ee557a57263ab4a
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-12-19 18:58:31 -0800

    Commit

--- Prankster/Controller/Image/ImageCategoryVC.swift
+++ Prankster/Controller/Image/ImageCategoryVC.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ImageCategoryVC: UIViewController {
+    
+    @IBOutlet weak var navigationbarView: UIView!
+    @IBOutlet weak var imageCharacterAllCollectionView: UICollectionView!
+    @IBOutlet weak var searchbar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +20,8 @@ class ImageCategoryVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnBackTapped(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
-    */
 
 }
